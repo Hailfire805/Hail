a964e1219044afb9a67fdb32b76b456270453f83
Hailfire805 <70313918+Hailfire805@users.noreply.github.com>
2022-03-28 12:01:53 -0700

    Update myFirstFunction.js

--- myFirstFunction.js
+++ myFirstFunction.js
@@ -10,46 +10,72 @@ const mostRecentdateHired = {
     "Month": 04,
     "Day": 08,
     "Year": 2021,
-    "Hire Date": "April 8th, 2021"
+    "Paid Date": "April 8th, 2021",
+    "Start Date": "August, 19th, 2020",
+    "Start Month": 08,
+    "Start Day": 19,
+    "Start Year": 2020
 };
 function getTimeEmployed() {
     if (dateToday.Employer == mostRecentdateHired.Employer) {
         var years = 0;
-        if (dateToday.Year == mostRecentdateHired.Year) {
-            years = 0;
-        } else {
-            dateToday.Year - mostRecentdateHired.Year;
-        };
+        if (dateToday.Year == mostRecentdateHired["Start Year"]) {
+                years = 0;
+            } else {
+                years = dateToday.Year - mostRecentdateHired["Start Year"];
+            };
         var months = 0; 
-        if (dateToday.Month < mostRecentdateHired.Month && dateToday.Year - mostRecentdateHired.Year == 1) {
-            months = dateToday.Month - mostRecentdateHired.Month + 12;
-        } else if (dateToday.Month < mostRecentdateHired.Month) {
-            years = years - 1;
-            months = dateToday.Month - mostRecentdateHired.Month + 12;
-        } else {
-            months = dateToday.Month - mostRecentdateHired.Month;
-        };
+        if (dateToday.Month < mostRecentdateHired["Start Month"] && dateToday.Year - mostRecentdateHired["Start Year"] == 1) {
+                months = dateToday.Month - mostRecentdateHired["Start Month"] + 12;
+            } else if (dateToday.Month < mostRecentdateHired["Start Month"]) {
+                years = years - 1;
+                months = dateToday.Month - mostRecentdateHired["Start Month"] + 12;
+            } else {
+                months = dateToday.Month - mostRecentdateHired["Start Month"];
+            };
         var days = 0;
-        if (dateToday.Day < mostRecentdateHired.Day) {
-            months = months - 1;
-            days = dateToday.Day - mostRecentdateHired.Day + 30;
-        } else {
-            days = dateToday.Day - mostRecentdateHired.Day;
-        };
+        if (dateToday.Day < mostRecentdateHired["Start Day"]) {
+                months = months - 1;
+                days = dateToday.Day - mostRecentdateHired["Start Day"] + 30;
+            } else {
+                days = dateToday.Day - mostRecentdateHired["Start Day"];
+            };
         let employmentDuration = "Time employed by " + dateToday.Employer + ": " + years + " Years " + months + " Months " + days + " Days";
-        return employmentDuration;
-    } else {
-        return "Unemployed";
+            return employmentDuration;
+        } else {
+            return "Unemployed";
     };
 };
-console.log(getTimeEmployed());
 
-let me = {
+
+const me = {
     "Name": "Brad",
     "Age": 25,
-    "Occupation": "Software Developer",
+    "Occupations": [
+        ["      1. [Completed: Internal Community Server Guru, ", 0],
+        ["      2. [Completed: Video Recording Artist,         ","18 to 20"],
+        ["      3. [On-Hold: Remote Team Manager,              ","20 to 25"],
+        ["      4. [On-Hold: Lead Video Editor,                ", 40],
+        ["      5. [Active: Junior Research Assistant,         ","25 to 40"],
+        ["      6. [Active: Technical Manager,                 ", 40]
+    ],
     "Employer": "Facecheck",
-    "Tenure": mostRecentdateHired["Hire Date"] + " - " + dateToday["Current Date"]
-}
+    "Tenure": mostRecentdateHired["Start Date"] + " - " + dateToday["Current Date"],
+    "Paid Time": mostRecentdateHired["Paid Date"] + " - " + dateToday["Current Date"]
+};
 
-console.log(me.Tenure);
+
+if (dateToday.Employer == mostRecentdateHired.Employer) {
+    let employmentStatus = "Employed";
+    console.log("Employment Status: " + employmentStatus);
+    console.log("Employer: " + dateToday.Employer);
+    console.log("  " + getTimeEmployed());
+    console.log("    Dates Employed: " + me.Tenure);
+    console.log("    Dates Paid: " + me["Paid Time"])
+    console.log("      Jobs: ");
+    for (let i = 0, x = me["Occupations"].length; i < x; i++) {
+        let jobTitle = me["Occupations"][i].shift();
+        let payRate =" Rate: $" + me["Occupations"][i].shift() + "/Hr";
+        console.log("   " + jobTitle + " " + payRate + "]");
+    };
+};
